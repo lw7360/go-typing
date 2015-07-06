593d696471b5f50b4acba557ecde1ac71ad893fe
Larry Wu <lw7360@gmail.com>
2015-07-05 21:55:13 -0700

    Add timetracking to Game struct

--- game.go
+++ game.go
@@ -3,11 +3,16 @@ package main
 import (
 	"io/ioutil"
 	"strings"
+	"time"
 )
 
 type Game struct {
-	wordList []string
-	stats    Stats
+	wordList  []string  // Slice of words to practice
+	curStats  Stats     // Stats for current session
+	stats     Stats     // Stats for current session + all past sessions
+	curWord   int       // Index of current word in wordList
+	curChar   int       // Index of current char of current word
+	startTime time.Time // Start time of current session
 }
 
 func (g *Game) loadWords(filename string) bool {
@@ -23,6 +28,14 @@ func (g *Game) loadStats(filename string) bool {
 	return g.stats.loadStats(filename)
 }
 
+func (g *Game) initTime() {
+	g.startTime = time.Now()
+}
+
+func (g *Game) gameTime() float64 {
+	return time.Since(g.startTime).Seconds()
+}
+
 func NewGame(wordsFile string, statsFile string) *Game {
 	g := new(Game)
 	if !g.loadWords(wordsFile) {
@@ -31,6 +44,5 @@ func NewGame(wordsFile string, statsFile string) *Game {
 	if !g.loadStats(statsFile) {
 		panic("Failed to load statsFile: \"" + statsFile + "\"")
 	}
-
 	return g
 }
